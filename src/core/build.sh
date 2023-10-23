eb2127b4ba99dd69183c3edcf9abd8b5b51d0b1a
Edward Pinkston <edward.pinkston@gmail.com>
2023-10-22 23:22:54 -0500

    Commit build fix

--- src/core/build.sh
+++ src/core/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-em++ --bind ESEmu.cpp mappers/Mapper.cpp mappers/Mapper000.cpp mappers/Mapper001.cpp mappers/Mapper002.cpp mappers/Mapper003.cpp mappers/Mapper004.cpp mappers/Mapper007.cpp mappers/Mapper066.cpp CPU.cpp APU.cpp Bus.cpp Cart.cpp PPU.cpp Util.cpp -O2 -s EXPORT_ES6=1 -s ALLOW_MEMORY_GROWTH=1 -s ENVIRONMENT=web -s MODULARIZE=1 -s EXPORTED_FUNCTIONS=['_malloc', '_free'] -s ASSERTIONS=1
+em++ --bind ESEmu.cpp mappers/Mapper.cpp mappers/Mapper000.cpp mappers/Mapper001.cpp mappers/Mapper002.cpp mappers/Mapper003.cpp mappers/Mapper004.cpp mappers/Mapper007.cpp mappers/Mapper066.cpp CPU.cpp APU.cpp Bus.cpp Cart.cpp PPU.cpp Util.cpp -O2 -s EXPORT_ES6=1 -s ALLOW_MEMORY_GROWTH=1 -s ENVIRONMENT=web -s MODULARIZE=1 -s EXPORTED_FUNCTIONS=['_malloc','_free'] -s ASSERTIONS=1
