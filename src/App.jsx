610b98cf39914e70cdc2044394f5a827fe1463c0
Edward Pinkston <edward.pinkston@gmail.com>
2023-10-22 20:16:45 -0500

    Attempt callback based rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,44 @@ import './App.css'
 
 function App() {
   const [emu, setEmu] = useState(new window.emuModule.ESEmu());
+  const [renderFrame, setRenderFrame] = useState(false);
 
   const canvasRef = useRef(null);
 
   const canvasWidth = 512;
   const canvasHeight = 480;
 
-  useEffect(() => {
-    const pxVec = emu.getFrameBuffer();
-    const pxArr = new Uint8Array(pxVec.size());
-    for (let i = 0; i < pxArr.length; i++) {
-      pxArr[i] = pxVec.get(i);
-    }
+  const renderFrameCallback = () => {
+    setInterval(() => {
+      setRenderFrame(true);
+    }, 16)
+  }
 
-    const scaleFactor = canvasWidth / 256;
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    const imgData = ctx.createImageData(256, 240);
-    imgData.data.set(pxArr);
-    ctx.putImageData(imgData, 0, 0, 0, 0, canvasWidth, canvasHeight);
-    ctx.scale(scaleFactor, scaleFactor);
-    ctx.drawImage(canvas, 0, 0, canvasWidth, canvasHeight);
-  }, [canvasRef]);
-  
+  // useEffect(() => {
+    if (renderFrame) {
+      // console.log("rendering frame");
+      const pxVec = emu.getFrameBuffer();
+      const pxArr = new Uint8Array(pxVec.size());
+      for (let i = 0; i < pxArr.length; i++) {
+        pxArr[i] = pxVec.get(i);
+      }
+
+      const scaleFactor = canvasWidth / 256;
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      const imgData = ctx.createImageData(256, 240);
+      imgData.data.set(pxArr);
+      ctx.putImageData(imgData, 0, 0, 0, 0, canvasWidth, canvasHeight);
+      ctx.scale(scaleFactor, scaleFactor);
+      ctx.drawImage(canvas, 0, 0, canvasWidth, canvasHeight);
+
+      setRenderFrame(false);
+    }
+  // }, [renderFrame]);
+
+  useEffect(() => {
+    renderFrameCallback();
+  }, [])
 
   return (
     <>
