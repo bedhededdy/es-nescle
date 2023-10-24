efc185a317e12badf12b4cb26897b0fc284353d6
Edward Pinkston <edward.pinkston@gmail.com>
2023-10-23 19:31:42 -0500

    Show real framebuffer on backend

--- src/core/ESEmu.cpp
+++ src/core/ESEmu.cpp
@@ -10,19 +10,18 @@ namespace NESCLE {
 std::vector<uint8_t> ESEmu::GetFrameBuffer() {
     // FIXME: USE FRAMEBUFFER PROPERLY
     // FIXME: THIS IS LEAKING INFINITY MEMORY
-    std::cout << "called\n";
     constexpr int size = 256 * 240 * 4;
     std::vector<uint8_t> res(size);
     uint32_t* frame_buffer = nes.GetPPU().GetFramebuffer();
     for (int i = 0; i < size; i += 4) {
-        // res[i+0] = frame_buffer[i/4] & 0x00ff0000;
-        // res[i+1] = frame_buffer[i/4] & 0x0000ff00;
-        // res[i+2] = frame_buffer[i/4] & 0x000000ff;
-        // res[i+3] = frame_buffer[i/4] & 0xff000000;
-        res[i] = 0xff;
-        res[i+1] = 0;
-        res[i+2] = 0;
-        res[i+3] = 0xff;
+        res[i+0] = frame_buffer[i/4] & 0x00ff0000;
+        res[i+1] = frame_buffer[i/4] & 0x0000ff00;
+        res[i+2] = frame_buffer[i/4] & 0x000000ff;
+        res[i+3] = frame_buffer[i/4] & 0xff000000;
+        // res[i] = 0xff;
+        // res[i+1] = 0;
+        // res[i+2] = 0;
+        // res[i+3] = 0xff;
     }
     return res;
 }
