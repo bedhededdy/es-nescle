a5770fbe085c434d0501f78929fb18cd96557145
Edward Pinkston <edward.pinkston@gmail.com>
2023-10-23 20:12:39 -0500

    Shift bits appropriately

--- src/core/ESEmu.cpp
+++ src/core/ESEmu.cpp
@@ -15,13 +15,13 @@ std::vector<uint8_t> ESEmu::GetFrameBuffer() {
     uint32_t* frame_buffer = nes.GetPPU().GetFramebuffer();
     for (int i = 0; i < size; i += 4) {
         if (run_emulation) {
-            res[i+0] = frame_buffer[i/4] & 0x00ff0000;
-            res[i+1] = frame_buffer[i/4] & 0x0000ff00;
+            res[i+0] = (frame_buffer[i/4] & 0x00ff0000) >> 16;
+            res[i+1] = (frame_buffer[i/4] & 0x0000ff00) >> 8;
             res[i+2] = frame_buffer[i/4] & 0x000000ff;
-            res[i+3] = frame_buffer[i/4] & 0xff000000;
+            res[i+3] = (frame_buffer[i/4] & 0xff000000) >> 24;
 
-            if (frame_buffer[i/4] != 0)
-                std::cout << "we have something to show\n";
+            // if (res[i+3] != 0)
+                // std::cout << "we have something to show\n";
         } else {
             res[i] = 0xff;
             res[i+1] = 0;
