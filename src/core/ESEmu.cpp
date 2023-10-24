d2844258e71b233bbea56ec22f1444d268dc2c56
Edward Pinkston <edward.pinkston@gmail.com>
2023-10-23 19:54:23 -0500

    Try real rendering

--- src/core/ESEmu.cpp
+++ src/core/ESEmu.cpp
@@ -14,14 +14,17 @@ std::vector<uint8_t> ESEmu::GetFrameBuffer() {
     std::vector<uint8_t> res(size);
     uint32_t* frame_buffer = nes.GetPPU().GetFramebuffer();
     for (int i = 0; i < size; i += 4) {
-        res[i+0] = frame_buffer[i/4] & 0x00ff0000;
-        res[i+1] = frame_buffer[i/4] & 0x0000ff00;
-        res[i+2] = frame_buffer[i/4] & 0x000000ff;
-        res[i+3] = frame_buffer[i/4] & 0xff000000;
-        // res[i] = 0xff;
-        // res[i+1] = 0;
-        // res[i+2] = 0;
-        // res[i+3] = 0xff;
+        if (run_emulation) {
+            res[i+0] = frame_buffer[i/4] & 0x00ff0000;
+            res[i+1] = frame_buffer[i/4] & 0x0000ff00;
+            res[i+2] = frame_buffer[i/4] & 0x000000ff;
+            res[i+3] = frame_buffer[i/4] & 0xff000000;
+        } else {
+            res[i] = 0xff;
+            res[i+1] = 0;
+            res[i+2] = 0;
+            res[i+3] = 0xff;
+        }
     }
     return res;
 }
@@ -32,18 +35,18 @@ bool ESEmu::LoadROM(uintptr_t buf_as_ptr) {
 
 void ESEmu::Clock() {
     if (run_emulation) {
-        // while (!nes.GetPPU().GetFrameComplete()) {
-        //     nes.Clock();
-        // }
-        // nes.GetPPU().ClearFrameComplete();
-        for (int i = 0; i < 500; i++) {
-            do {
-                nes.GetCPU().Clock();
-            } while (nes.GetCPU().GetCyclesRem() > 0);
-            nes.GetCPU().Clock();
+        while (!nes.GetPPU().GetFrameComplete()) {
+            nes.Clock();
         }
+        nes.GetPPU().ClearFrameComplete();
+        // for (int i = 0; i < 500; i++) {
+        //     do {
+        //         nes.GetCPU().Clock();
+        //     } while (nes.GetCPU().GetCyclesRem() > 0);
+        //     nes.GetCPU().Clock();
+        // }
 
-        run_emulation = false;
+        // run_emulation = false;
     }
 }
 
